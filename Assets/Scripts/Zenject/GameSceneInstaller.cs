d39695be0cd4e01b503499892b4ed069f8f89666
İbrahim Ethem Kurugöl <ibrahimethemkurugol@gmail.com>
2025-04-22 22:52:19 +0300

    Zenject DeclareSignal and Container.Bind

--- Assets/Scripts/Zenject/GameSceneInstaller.cs
+++ Assets/Scripts/Zenject/GameSceneInstaller.cs
@@ -7,16 +7,28 @@ namespace Injection
     {
         [SerializeField] private InputController inputController;
         [SerializeField] private CharacterMovementController characterMovementController;
+        [SerializeField] private ObjectPool objectPool;
+        [SerializeField] private PlatformController platformController;
+        [SerializeField] private GameController gameController;
 
         public override void InstallBindings()
         {
             //Signals
             SignalBusInstaller.Install(Container);
             Container.DeclareSignal<TapEvent>();
+            Container.DeclareSignal<GameStartedEvent>();
+            Container.DeclareSignal<GameOverEvent>();
+            Container.DeclareSignal<GameSuccessEvent>();
+            Container.DeclareSignal<PerfectPlacementEvent>();
+            Container.DeclareSignal<NormalPlacementEvent>();
+
 
             //Bindings
             Container.Bind<InputController>().FromInstance(inputController).AsSingle();
             Container.Bind<CharacterMovementController>().FromInstance(characterMovementController).AsSingle();
+            Container.Bind<ObjectPool>().FromInstance(objectPool).AsSingle();
+            Container.Bind<PlatformController>().FromInstance(platformController).AsSingle();
+            Container.Bind<GameController>().FromInstance(gameController).AsSingle();
         }
     }
 }

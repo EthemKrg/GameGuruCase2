1293e7bc8fb650e8387925e76238087d19692da1
İbrahim Ethem Kurugöl <ibrahimethemkurugol@gmail.com>
2025-04-22 20:39:46 +0300

    CharacterMovement script added

---
+++ Assets/Scripts/CharacterMovementController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    // Speed of the player movement
+    public float speed = 5f;
+
+    [SerializeField] private Rigidbody rb;
+
+    private bool canMove = false;
+    public bool CanMove
+    {
+        get { return canMove; }
+        set
+        {
+            canMove = value;
+            if (!canMove)
+            {
+                rb.velocity = Vector3.zero; // Stop movement when canMove is false
+            }
+        }
+    }
+
+    /// <summary>
+    /// Called once per frame. Updates the player's movement if canMove is true.
+    /// </summary>
+    void Update()
+    {
+        if (canMove)
+        {
+            // Set the velocity to move the player in a straight direction
+            rb.velocity = transform.forward * speed;
+        }
+        else
+        {
+            // Stop the player's movement
+            rb.velocity = Vector3.zero;
+        }
+    }
+}

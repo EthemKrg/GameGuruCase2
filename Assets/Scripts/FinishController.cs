13a4d63c19333dbd629a6650973d9b65125d730a
İbrahim Ethem Kurugöl <ibrahimethemkurugol@gmail.com>
2025-04-23 02:32:47 +0300

    Finish controller added

---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using Injection;
+using UnityEngine;
+using Zenject;
+
+/// <summary>
+/// Handles the finish line logic in the game.
+/// </summary>
+/// <remarks>
+/// This class is responsible for detecting when the player reaches the finish line and triggering the success event.
+/// </remarks>
+public class FinishController : MonoBehaviour
+{
+    [Inject] private SignalBus signalBus;
+    [Inject] private PlatformSpawner platformSpawner;
+
+    [SerializeField] private int platformDistanceToFinish = 5; // Distance to the finish line
+    public int PlatformDistanceToFinish => platformDistanceToFinish; // Public getter for the distance to the finish line
+
+    private Tween moveTweenX;
+
+    private void Start()
+    {
+        float zSize = platformSpawner.PlatformSize.z;
+        transform.position = new Vector3(0, platformSpawner.NextPlatformPosition.y,
+            zSize * platformDistanceToFinish - 0.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            signalBus.Fire(new GameSuccessEvent());
+        }
+    }
+
+    public void MoveToXPos(float xPos)
+    {
+        if (moveTweenX != null)
+        {
+            moveTweenX.Kill(); // Kill the previous tween if it exists
+        }
+
+        moveTweenX = transform.DOMoveX(xPos, 1f);
+    }
+}
